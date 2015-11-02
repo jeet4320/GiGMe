f68def14ecbf7621f891ad26de8b5dbc3bea120b
Tirumala <ks.tirumala@outlook.com>
2015-11-01 19:59:15 -0600

    .

--- app/src/main/java/gigme/uta/com/gigme/Register.java
+++ app/src/main/java/gigme/uta/com/gigme/Register.java
@@ -16,7 +16,7 @@ public class Register extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_register, menu);
+        //getMenuInflater().inflate(R.menu.menu_register, menu);
         return true;
     }
 
