572a357583256157cb5252b1154c223e9fe6c287
Tirumala <ks.tirumala@outlook.com>
2015-11-01 21:57:10 -0600

    .

--- app/src/main/java/gigme/uta/com/gigme/Events.java
+++ app/src/main/java/gigme/uta/com/gigme/Events.java
@@ -102,6 +102,21 @@ public class Events extends Activity
         }
 
         @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            // Handle action bar item clicks here. The action bar will
+            // automatically handle clicks on the Home/Up button, so long
+            // as you specify a parent activity in AndroidManifest.xml.
+            int id = item.getItemId();
+
+            //noinspection SimplifiableIfStatement
+            if (id == R.id.action_example) {
+                return true;
+            }
+
+            return super.onOptionsItemSelected(item);
+        }
+
+        @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_events, container, false);
